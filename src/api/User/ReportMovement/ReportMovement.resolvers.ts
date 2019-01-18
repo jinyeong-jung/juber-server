472fc0ef7d933660d4f01974f89506be5f914938
jinyeong-jung <jinyeong32@gmail.com>
2019-01-18 16:28:53 +0900

    ReportMovement Resolver

---
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -0,0 +1,37 @@
+import { Resolvers } from "src/types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import {
+  ReportMovementMutationArgs,
+  ReportMovementResponse
+} from "src/types/graph";
+import User from "../../../entities/User";
+import cleanNullArgs from "../../../utils/cleanNullArg";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    ReportMovement: privateResolver(
+      async (
+        _,
+        args: ReportMovementMutationArgs,
+        { req }
+      ): Promise<ReportMovementResponse> => {
+        const user: User = req.user;
+        const notNull = cleanNullArgs(args);
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
