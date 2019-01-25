5e465b8b4191d78460237b86c3a59cbea7a8facf
jinyeong-jung <jinyeong32@gmail.com>
2019-01-25 14:53:37 +0900

    GetChat Resolver

---
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -0,0 +1,52 @@
+import Chat from "../../../entities/Chat";
+import User from "../../../entities/User";
+import { GetChatQueryArgs, GetChatResponse } from "src/types/graph";
+import { Resolvers } from "src/types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetChat: privateResolver(
+      async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
+        const user: User = req.user;
+        try {
+          const chat = await Chat.findOne(
+            {
+              id: args.chatId
+            },
+            { relations: ["messages"] }
+          );
+          if (chat) {
+            if (user.id === chat.driverId || user.id === chat.passengerId) {
+              return {
+                ok: true,
+                error: null,
+                chat
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not authorized to see this chat",
+                chat: null
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Chat not found",
+              chat: null
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            chat: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
