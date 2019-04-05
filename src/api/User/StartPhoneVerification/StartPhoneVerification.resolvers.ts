f5610d534d85fc5099da483ee7af376721b2708b
jinyeong-jung <jinyeong32@gmail.com>
2019-04-05 16:24:18 +0900

    Bugfix - add User on startPhoneVerification

--- src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
+++ src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -5,6 +5,7 @@ import {
 } from "src/types/graph";
 import Verification from "../../../entities/Verification";
 import { sendVerificationSMS } from "../../../utils/sendSMS";
+import User from "../../../entities/User";
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -23,6 +24,13 @@ const resolvers: Resolvers = {
         const newVerification = await Verification.create({
           target: "PHONE",
           payload: phoneNumber
+        }).save();
+        await User.create({
+          firstName: "firstName",
+          lastName: "lastName",
+          phoneNumber,
+          profilePhoto:
+            "https://lh3.googleusercontent.com/-CTwXMuZRaWw/AAAAAAAAAAI/AAAAAAAAAUg/8T5nFuIdnHE/photo.jpg"
         }).save();
         await sendVerificationSMS(newVerification.payload, newVerification.key);
         return {
